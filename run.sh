caf90e1d16b022d3a1734ba3e5b73cc1399c18b8
Daniel Yonkeu-Cheunko <daniel.yonkeucheunko@gmail.com>
2025-08-20 00:07:34 -0400

    basic TCP server

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ set -e
 # Project root
 PROJECT_ROOT=$(pwd)
 BUILD_DIR="$PROJECT_ROOT/build"
-BINARY_NAME="project"  # Must match the name in CMakeLists.txt
+BINARY_NAME="RedisClone"  # Must match the name in CMakeLists.txt
 
 # Create build folder if it doesn't exist
 mkdir -p "$BUILD_DIR"
@@ -18,4 +18,7 @@ cmake -S "$PROJECT_ROOT" -B "$BUILD_DIR"
 cmake --build "$BUILD_DIR" --verbose
 
 # Run the binary with any arguments passed to the script
-"$BUILD_DIR/$BINARY_NAME" "$@"
+# "$BUILD_DIR/$BINARY_NAME" "$@"
+
+gnome-terminal -- bash -c "$BUILD_DIR/Server; exec bash"
+gnome-terminal -- bash -c "$BUILD_DIR/Client; exec bash"

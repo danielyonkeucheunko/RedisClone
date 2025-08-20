a9c3a88abdc03c01afaa9f4ba6f22793d92ec187
Daniel Yonkeu <daniel.yonkeucheunko@gmail.com>
2025-08-19 21:35:12 -0400

    Initial commit

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Exit immediately if a command fails
+set -e
+
+# Project root
+PROJECT_ROOT=$(pwd)
+BUILD_DIR="$PROJECT_ROOT/build"
+BINARY_NAME="project"  # Must match the name in CMakeLists.txt
+
+# Create build folder if it doesn't exist
+mkdir -p "$BUILD_DIR"
+
+# Configure CMake
+cmake -S "$PROJECT_ROOT" -B "$BUILD_DIR"
+
+# Build the project
+cmake --build "$BUILD_DIR" --verbose
+
+# Run the binary with any arguments passed to the script
+"$BUILD_DIR/$BINARY_NAME" "$@"
